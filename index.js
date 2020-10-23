6cdad05b9228654819cff165ba0332bd6912282a
Danial Hasan <danialhasan14@gmail.com>
2020-10-23 14:21:34 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-app.listen(5500, () => console.log('Listening at port ' + 5500));
+app.listen(5504, () => console.log("listening to port 5504"));
 app.use(express.static('public'));
 app.use((req, res, next) => {
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
-    res.setHeader("Allow: GET, POST, HEAD");
-    next();
-})
-// app.use(express.json({
-//     limit: '1mb'
-// }));
+    res.setHeader('Access-Control-Allow-Credentials', 'omit');
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.end()
 
-app.get("/api", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    console.log(req);
+    next();
+
+})
+app.post('/api', (req, res) => {
+    console.log("received request");
+    console.log(req.body);
 });
