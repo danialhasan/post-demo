1aecd425dfd7ac9e42d9fe562eb82540abb3ccc0
Danial Hasan <danialhasan14@gmail.com>
2020-11-01 21:57:17 -0500

    added 'return entries of last [x] minutes

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-"use strict";
 const express = require("express");
 const app = express();
 const mongo = require('mongodb').MongoClient
 const datastore = require("nedb");
 
 // var ts
-var lastTimeGetRequestWasMade = 0;
-var currentDate;
 app.listen(5500, () => console.log("listening to port 5500"));
 app.use(express.static('public'));
 app.use(express.json({
@@ -15,6 +12,16 @@ app.use(express.json({
 }))
 var database = new datastore('database.db');
 database.loadDatabase();
+
+//sub minutes from current time
+function submin(dt, minutes) {
+    return new Date(dt.getTime() - minutes * 60000);
+}
+/*
+    usage:
+    console.log(addmin(new Date(2014, 10, 2), 683).toString());
+*/
+
 
 // var i = 0;
 app.use((req, res, next) => {
@@ -31,39 +38,43 @@ app.use((req, res, next) => {
     next();
 
 });
-app.get('/api', function (req, res) {
-    currentDate = new Date();
-    // var test = "testing";
-    //on get request, send the latest data that was sent to the server. 
-    // database.find({
-    //     fname: 'danial'
-    // }, function (err, docs) {
-    //     // docs is an array containing documents Mars, Earth, Jupiter
-    //     // If no document is found, docs is equal to []
-    //     // return docs
-    //     if (err) {
-    //         res.statusCode = 404;
-    //         res.json = err;
-    //     };
-    //     res.json(docs);
-    // });
-    // res.send("test");
+app.get('/latestindex1min', (req, res) => {
+    // var oneminagodate = submin(new Date(), 1);
+    var oneminagoseconds = submin(new Date(), 1).getTime()
+    //convert to milliseconds since january 1987
+    database.find({
+        timestamp: {
+            // $gte: oneminago
+            $gte: oneminagoseconds
+        }
+    }).sort({
+        timestamp: -1
+    }).exec((err, docs) => {
+        res.json({
+            docs: docs
+        });
+    })
 
-    // database.findOne({}, {
-    //     sort: {
-    //         _id: -1
-    //     },
-    //     limit: 1
-    // });
+})
 
-    // database.findOne({
-    //     collection: 'names'
-    // }).sort({
-    //     _id: -1
-    // }).exec(function (err, docs) {
-    //     // docs is [doc1, doc3, doc2]
-    //     res.json(docs);
-    // });
+app.get('/latestindex2min', (req, res) => {
+    var twominagoseconds = submin(new Date(), 2).getTime()
+
+    database.find({
+        timestamp: {
+            // $gte: oneminago
+            $gte: twominagoseconds
+        }
+    }).sort({
+        timestamp: -1
+    }).exec((err, docs) => {
+        res.json({
+            docs: docs
+        });
+    })
+})
+app.get('/latestindex', function (req, res) {
+
     database.find({
         //find all entries with a timestamp greater than the previous, last-seen timestamp
         // timestamp: {
@@ -71,37 +82,34 @@ app.get('/api', function (req, res) {
         //     $gt: timestamp2
         //     //last_seen_timestamp is the timestamp of the second last entry.
         // }
-        lname: 'thisisthelastname',
+        // lname: 'thisisthelastname',
         timestamp: {
-            $gt: lastTimeGetRequestWasMade,
-            $ls: new Date()
+            $gt: 0 //,
+            // $ls: new Date()
             //WONT WORK UNTIL THERE HAS BEEN A POST REQUEST SUBMITTED. THIS IS BECAUSE
             //WHEN FORM IS REFRESHED, TS = UNDEFINED
         }
 
     }).sort({
         timestamp: -1
-        // createdAt: -1
     }).exec((err, docs) => {
-        // docs is[doc1, doc3, doc2]
         res.json({
-            docs,
-            // ts
+            docs
         });
     })
-    lastTimeGetRequestWasMade = new Date();
+    // lastTimeGetRequestWasMade = new Date();
 
-    // database.find({
-    //     collection: 'names'
-    // })
+})
+app.get('/delete', (req, res) => {
+    database.remove({}, {
+        multi: true
+    }, (err, numRemoved) => {
+        res.json({
+            message: "Database wiped."
+        })
+    })
 })
 app.post('/frontend', (req, res) => {
-    //when a post is made to domain.com/frontend, take the timestamp of the request and hold it. It will be used in 
-    //get purposes. 
-
-    //in the get handler, find all db entries with a timestamp higher than the timestamp recieved by the post. 
-    //Submit them. If there are none, nothing will be submitted. If there are any, all of them over the timestamp 
-    //written will be sent. 
 
     console.log("received request");
     // i++;
